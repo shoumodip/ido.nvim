152e88f5a958387d0af9544d9cac05190e0e8cf4
Shoumodip Kar <shoumodipkar@gmail.com>
2022-05-01 20:56:57 +0530

    [std] fix: show the `std` prefix for the error messages

--- lua/ido/std.lua
+++ lua/ido/std.lua
@@ -142,7 +142,7 @@ function std.check_inside_git(name)
 end
 
 function std.git_files()
-    if std.check_inside_git("git_files") then
+    if std.check_inside_git("std.git_files") then
         local file = ido.start(vim.fn.systemlist("git ls-files --cached --others --exclude-standard"), {prompt = "Git Files: "})
         if file then
             vim.cmd("edit "..file)
@@ -151,7 +151,7 @@ function std.git_files()
 end
 
 function std.git_diff()
-    if std.check_inside_git("git_diff") then
+    if std.check_inside_git("std.git_diff") then
         local file = ido.start(vim.fn.systemlist("git diff --name-only"), {prompt = "Git Diff: "})
         if file then
             vim.cmd("edit "..file)
@@ -160,9 +160,9 @@ function std.git_diff()
 end
 
 function std.git_log()
-    if std.check_inside_git("git_log") then
+    if std.check_inside_git("std.git_log") then
         if vim.fn.exists("*FugitiveFind") == 0 then
-            vim.api.nvim_err_writeln("git_log: tpope/vim-fugitive not installed")
+            vim.api.nvim_err_writeln("std.git_log: tpope/vim-fugitive not installed")
         else
             local commit = ido.start(vim.fn.systemlist("git log --format='%h%d %s %cr'"), {prompt = "Git Log: "})
             if commit then
@@ -174,7 +174,7 @@ function std.git_log()
 end
 
 function std.git_status()
-    if std.check_inside_git("git_status") then
+    if std.check_inside_git("std.git_status") then
         local file = ido.start(vim.fn.systemlist("git status --short --untracked-files=all"), {prompt = "Git Status: "})
 
         if file then
